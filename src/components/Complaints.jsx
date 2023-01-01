66a0cb457d24098974baf191756d2bc98a242e10
Abdul Rafay <farhatiqbal@protonmail.com>
2023-01-02 04:30:51 +0500

    added view & admin

--- src/components/Complaints.jsx
+++ src/components/Complaints.jsx
@@ -41,7 +41,7 @@ export default function Complaints() {
                 setFilter(e.target.value);
               }}
               className="block border w-full p-3 pl-10 text-sm border-gray-200 rounded-md focus:border-blue-500 focus:ring-blue-500 w-full"
-              placeholder="Search..."
+              placeholder="Enter Your Complaint Id"
             />
             <div className="absolute inset-y-0 left-0 flex items-center pl-4 pointer-events-none">
               <svg
